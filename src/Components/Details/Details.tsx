6ed68ca895c6c52f48bd010270cb0f33f66b295a
LeoGonzaga <sacredlotus05@gmail.com>
2023-04-22 22:31:15 -0300

    feat: removing power

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -3,7 +3,6 @@ import * as SC from "./styles";
 import Name from "../Name";
 import Type from "../Type";
 import Describe from "../Describe";
-import Powers from "../../components/Powers";
 
 export const Details = (): JSX.Element => {
   return (
